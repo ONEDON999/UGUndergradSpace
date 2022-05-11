1c5bf5fc07361e981746e860426cecd434763ca0
Nii-Gyan Christian <niigyanchristian@gmail.com>
2022-05-11 15:07:15 +0000

    added slides

--- UGUndergradSpace/settings.py
+++ UGUndergradSpace/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ugspace',
         'USER': 'postgres',
-        'PASSWORD': 'jesusC123',
+        'PASSWORD': 'onedon',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
